593b97d6f9b84a1a9a73d811a0959ca8a2d87e32
Emirhan Bulut <emirhanblt9@gmail.com>
2023-10-25 18:20:19 +0300

    SessionFactoryBuilder class, implements Builder Interface

---
+++ system/src/main/java/com/service/util/builders/conc/SessionFactoryBuilder.java
@@ -0,0 +1,19 @@
+package com.service.util.builders.conc;
+
+import com.service.util.builders.Builder;
+import com.service.util.exceptions.InitializerException;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class SessionFactoryBuilder implements Builder<SessionFactory> {
+    @Override
+    public SessionFactory build() throws InitializerException {
+        try{
+            Configuration config = new Configuration().configure("hibernate.cfg.xml");//Create a config with hibernate config
+            return config.buildSessionFactory();//Build the factory and return it
+        } catch (Throwable err) {
+            System.out.println("Somethings went wrong on create SessionFactory");
+            throw new InitializerException(err.getMessage());//Throw custom exception on error
+        }
+    }
+}
