98df587576b6cdaa4d9d913a2aa365608932753d
Emirhan Bulut <emirhanblt9@gmail.com>
2023-10-30 02:48:37 +0300

    It is initialized

---
+++ system/src/main/java/com/service/util/builders/conc/MailBuilder.java
@@ -0,0 +1,11 @@
+package com.service.util.builders.conc;
+
+import com.service.model.Mail;
+import com.service.util.builders.Builder;
+
+public class MailBuilder implements Builder<Mail> {
+    @Override
+    public Mail build() {
+        return null;
+    }
+}
