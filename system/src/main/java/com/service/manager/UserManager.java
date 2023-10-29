2a6c4e6dbfc5716f5d05f98c389cb5a23149f720
Emirhan Bulut <emirhanblt9@gmail.com>
2023-10-30 02:51:14 +0300

    Dependency Injected

---
+++ system/src/main/java/com/service/manager/UserManager.java
@@ -0,0 +1,23 @@
+package com.service.manager;
+
+import com.service.daccess.DataAccess;
+import com.service.model.User;
+
+import java.util.List;
+
+public class UserManager {
+    private DataAccess<User> dataAccess;
+
+    public UserManager(DataAccess<User> dataAccess) {
+        this.dataAccess = dataAccess;
+    }
+
+    public User getById(int id){
+        return this.dataAccess.getById(id);
+    }
+
+    public List<User> getAll(){
+        return this.dataAccess.getAll();
+    }
+
+}
