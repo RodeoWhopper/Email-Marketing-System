47669cb0a9f26c690dc7e78aa92ecbe8d21c3bc2
Emirhan Bulut <emirhanblt9@gmail.com>
2023-10-30 15:35:51 +0300

    An Exception when there is an email address did not fit into email regex

---
+++ system/src/main/java/com/service/util/exceptions/mail/childs/AddressException.java
@@ -0,0 +1,13 @@
+package com.service.util.exceptions.mail.childs;
+
+import com.service.util.exceptions.mail.MailBuilderException;
+
+public class AddressException extends MailBuilderException {
+    public AddressException() {
+        super("Receiver or sender address is wrong");
+    }
+
+    public AddressException(String message) {
+        super(message);
+    }
+}
