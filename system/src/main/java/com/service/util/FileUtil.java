63981145050b070a455c0269677ee20dddd49857
Emirhan Bulut <emirhanblt9@gmail.com>
2023-10-31 14:37:53 +0300

    FileUtil class created for file reading

--- system/src/main/java/com/service/util/FileUtil.java
+++ system/src/main/java/com/service/util/FileUtil.java
@@ -1,7 +1,28 @@
 package com.service.util;
 
+import com.service.util.exceptions.file.ReadFileException;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 public class FileUtil {
-    public static String readHTMLFile(){
-        return null;
+    public static String readHTMLFile(String path) throws RuntimeException{
+        try{
+
+            StringBuilder stringBuilder = new StringBuilder();
+            BufferedReader reader = new BufferedReader(new FileReader(path));
+            String line = null;
+
+            while((line = reader.readLine()) != null){
+                stringBuilder.append(line);
+            }
+
+            return stringBuilder.toString();
+
+        } catch(Exception e){
+
+            throw new ReadFileException();
+            
+        }
     }
 }
