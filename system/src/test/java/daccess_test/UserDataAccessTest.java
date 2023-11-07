f7b1588d4315b3d31168f15984625820c5d8214c
Emirhan Bulut <emirhanblt9@gmail.com>
2023-11-07 17:48:06 +0300

    For now it only tests adding user to db

--- system/src/test/java/daccess_test/UserDataAccessTest.java
+++ system/src/test/java/daccess_test/UserDataAccessTest.java
@@ -1,9 +1,74 @@
 package daccess_test;
 
-import com.service.daccess.conc.UserDataAccess;
+import com.google.common.collect.Lists;
+import com.service.model.User;
+import com.service.util.builders.conc.SessionFactoryBuilder;
+import com.service.util.enums.PurchaseMeta;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.sql.Date;
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+import static com.service.util.enums.PurchaseMeta.DRESSING;
+import static com.service.util.enums.PurchaseMeta.TECHNOLOGY;
 
 public class UserDataAccessTest {
-    public static void main(String[] args) {
-        UserDataAccess dataAccess = new UserDataAccess();
+
+    private static SessionFactory sessionFactory;
+
+    @BeforeClass
+    public static void setUp(){
+        SessionFactoryBuilder sessionFactoryBuilder = new SessionFactoryBuilder();
+        sessionFactory = sessionFactoryBuilder.build();
+    }
+
+    @Test
+    public void testAddUser(){
+        
+        Date now = Date.valueOf(LocalDate.now());
+        ArrayList<PurchaseMeta> list = Lists.newArrayList(TECHNOLOGY,DRESSING);
+        User user = new User(
+                "Emirhan",
+                "Bulut",
+                "emirhan9@gmail.com",
+                now,
+                list);
+
+
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+
+        Long userId = (Long) session.save(user);
+
+        transaction.commit();
+        session.close();
+
+        Assert.assertNotNull(userId);
+
+        session = sessionFactory.openSession();
+        User addedUser = session.get(User.class,userId);
+        session.close();
+
+        Assert.assertNotNull(addedUser);
+        Assert.assertEquals("Emirhan",addedUser.getName());
+        Assert.assertEquals("Bulut",addedUser.getSurname());
+        Assert.assertEquals("emirhan9@gmail.com",addedUser.getEmailAddress());
+        Assert.assertEquals(now,addedUser.getLastPurchaseDate());
+        Assert.assertEquals(list,addedUser.getPurchaseMeta());
+
+    }
+
+    @AfterClass
+    public static void tearDown(){
+        if(sessionFactory != null){
+            sessionFactory.close();
+        }
     }
 }
