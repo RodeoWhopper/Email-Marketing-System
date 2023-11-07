bfc69c49557165792370ae352deffac5da7626a6
Emirhan Bulut <emirhanblt9@gmail.com>
2023-11-07 15:09:26 +0300

    update method filled in UserDataAccess

--- system/src/main/java/com/service/daccess/conc/UserDataAccess.java
+++ system/src/main/java/com/service/daccess/conc/UserDataAccess.java
@@ -3,6 +3,7 @@ package com.service.daccess.conc;
 import com.service.daccess.DataAccess;
 import com.service.model.User;
 import com.service.util.builders.conc.SessionFactoryBuilder;
+import com.service.util.exceptions.database.UpdateObjectCannotBeNullException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -34,4 +35,18 @@ public class UserDataAccess implements DataAccess<User> {
         session.close();
         return users;
     }
+
+    @Override
+    public void update(User user) {
+        if(user != null){
+            Session session = sessionFactory.openSession();
+            Transaction transaction = session.beginTransaction();
+            session.update(user);
+            session.close();
+        } else {
+            throw new UpdateObjectCannotBeNullException();
+        }
+    }
+
+
 }
