827bf12c276c24a3884969f0af2a8c66b3aaecb3
Emirhan Bulut <emirhanblt9@gmail.com>
2023-11-07 12:18:50 +0300

    moved

--- system/src/main/java/com/service/util/other/MailUtil.java
+++ system/src/main/java/com/service/util/other/MailUtil.java
@@ -1,12 +1,12 @@
-package com.service.util;
+package com.service.util.other;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class MailUtil {
     public static boolean isMailValid(String str){
-        String emailPattern = "^[A-Za-z0-9+_.-]+@(.+)$";
-        return stringValidator(emailPattern,str);
+        String emailRegex = "^[A-Za-z0-9+_.-]+@(.+)$";
+        return stringValidator(emailRegex,str);
     }
     public static boolean isAnIp(String str) {
         String ipPattern = "^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\." +
