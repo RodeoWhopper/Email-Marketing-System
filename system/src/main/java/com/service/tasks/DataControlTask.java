febc74404f95573b475954a8703065320837f3e9
Emirhan Bulut <emirhanblt9@gmail.com>
2023-11-07 12:11:13 +0300

    Method added for iterate user lists and send mail

--- system/src/main/java/com/service/tasks/DataControlTask.java
+++ system/src/main/java/com/service/tasks/DataControlTask.java
@@ -1,16 +1,21 @@
 package com.service.tasks;
 
 import com.service.manager.UserManager;
+import com.service.model.Mail;
 import com.service.model.User;
-import com.service.util.UserUtil;
+import com.service.util.enums.Topic;
+import com.service.util.other.JsonUtil;
+import com.service.util.other.UserUtil;
+import com.service.util.builders.conc.MailBuilder;
 import com.service.util.enums.Datum;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class DataControlTask implements Runnable {
+public class DataControlTask extends Thread {
 
     private UserManager userManager;
+    private static MailBuilder mailBuilder;
 
     @Override
     public void run() {
@@ -24,7 +29,31 @@ public class DataControlTask implements Runnable {
                 .filter(user -> UserUtil.checkForDate(user) == Datum.MORE_THAN_30)
                 .collect(Collectors.toList());
 
+        sendEmailsToUsersInList(fiveDays, Topic.FIVE_DAYS);
+        sendEmailsToUsersInList(thirtyDays, Topic.THIRTY_DAYS);
+
+
 
     }
+
+    private static void sendEmailsToUsersInList(List<User> list, Topic topic){
+
+        list.forEach(user -> {
+
+            String receiver = user.getEmailAddress(),
+                    sender = JsonUtil.getEmailAccount()[0],
+                    password = JsonUtil.getEmailAccount()[1],
+                    smtpServer = JsonUtil.getSmtpAddress()[0],
+                    smtpPort = JsonUtil.getSmtpAddress()[1];
+
+            Mail mail = mailBuilder.build(receiver,sender,password,smtpServer,smtpPort,topic.name());
+
+            SendMailTask sendMailTask = new SendMailTask(mail);
+            sendMailTask.run();
+
+        });
+    }
+
+
 
 }
